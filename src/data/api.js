931eb45006689b98fc604bafadd7e9bc20bcc1ce
developer <maslovaoksanaua@gmail.com>
2017-12-18 19:07:29 +0200

    add images to car

--- src/data/api.js
+++ src/data/api.js
@@ -6,16 +6,19 @@ const api = [
                 type: 'X5',
                 model: 'Crossover',
                 color: 'red',
+                image: 'http://www.bmw-avangard.ru/images/gallery/gallery_model_cars/cq5damresizedimg1185largetime1455703526909.jpg'
             },
             {
                 type: 'X6',
                 model: 'Crossover',
                 color: 'green',
+                image: 'https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/At%20a%20glance/m-sportpackage-slide-01.jpg/_jcr_content/renditions/cq5dam.resized.img.485.low.time1510609710464.jpg'
             },
             {
                 type: 'X3',
                 model: 'Crossover',
                 color: 'blue',
+                image: 'https://photos-3.carwow.co.uk/blog/1600/Deep-sea-blue.jpg'
             }],
     },
     {
@@ -25,16 +28,19 @@ const api = [
                 type: 'RX7',
                 model: 'Sedan',
                 color: 'red',
+                image: 'http://carrrsmag.com/data_images/models/mazda-rx-7/mazda-rx-7-02.jpg'
             },
             {
                 type: 'RX8',
                 model: 'Sedan',
                 color: 'green',
+                image: 'http://www.modelly.com/images/11880/mazda-rx-8-autoart.jpg'
             },
             {
                 type: 'RX9',
                 model: 'Sedan',
                 color: 'blue',
+                image: 'http://2.bp.blogspot.com/-CtFNBR3r0tU/VGsh3UErC1I/AAAAAAAACIo/MerUTC5Q5kU/s1600/2015%2BMazda%2BRX-9%2BDesign%2BReview.jpg'
             }]
     },
     {
@@ -44,16 +50,19 @@ const api = [
                 type: 'S',
                 model: 'Sedan',
                 color: 'red',
+                image: 'http://blog.vehiclejar.com/wp-content/uploads/2014/12/Red-Mercedes-S-Class-by-Brabus-for-Santa-1.jpg'
             },
             {
                 type: 'E',
                 model: 'Sedan',
                 color: 'green',
+                image: 'https://s1.cdn.autoevolution.com/images/news/blue-green-chameleon-wrap-on-mercedes-e-class-44501-7.jpg'
             },
             {
                 type: 'ML',
                 model: 'Crossover',
                 color: 'blue',
+                image: 'https://top-car.ru/upload/gallery/big/p193h1hmepalc1uoo7quel11plv2.jpg'
             }]
     }];
 
