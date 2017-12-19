54fc4c53000e82c22f0f492f909d2b7678830d14
Alex Melnyk <ask.melnyk@gmail.com>
2017-12-19 16:27:51 +0200

    Refactored project files.

--- src/store/reducers/carReducer.js
+++ src/store/reducers/carReducer.js
@@ -1,32 +1,19 @@
 import * as CarActions from '../actions/carActions.js'
 
 const defaultState = {
-    list: [],
-    selected: null
+    list: []
 };
 
 export default (state = defaultState, action) => {
     switch (action.type) {
         case CarActions.ACTION_CAR_GET_LIST:
             return {
-                ...state,
                 ...action.payload
             };
         case CarActions.ACTION_CAR_ADD:
             return {
-                ...state,
-                list: [
-                    ...state.list,
-                    ...action.payload
-                ]
-            };
-        case CarActions.ACTION_CAR_GET_NORMALIZE_LIST:
-            return {
-                ...action.payload
-            };
-        case CarActions.ACTION_CAR_GET_BY_ID:
-            return {
-                ...action.payload
+                // ...state,
+                // list: action.payload.coordinates
             };
         case CarActions.ACTION_CAR_EDIT:
             return {
