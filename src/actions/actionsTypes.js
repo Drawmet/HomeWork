56977c3e4f7b6aaf079a6c3ef346b590ef332024
developer <maslovaoksanaua@gmail.com>
2017-12-19 15:11:35 +0200

    Store all cars with coordinates in Redux

--- src/actions/actionsTypes.js
+++ src/actions/actionsTypes.js
@@ -1,5 +1,17 @@
 export default {
+    GET_COORDINATES_CAR : "GET_COORDINATES_CAR",
     ADD_CAR: "ADD_CAR",
     EDIT_CAR: "EDIT_CAR",
-    DELETE_CAR: "DELETE_CAR"
+    DELETE_CAR: "DELETE_CAR",
+    GET_LIST_CARS: "GET_LIST_CARS",
 }
+
+// export const getAddCar = () => (dispatch) => {
+//     fetch('../data/api.js').then((name, properties) => coordinates.json())
+//         .then((coordinates) => {
+//             dispatch({
+//                 type: GET_COORDINATES_CAR,
+//                 payload: coordinates
+//             })
+//         })
+// };
