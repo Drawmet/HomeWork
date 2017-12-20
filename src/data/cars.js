e7d1e4ff586859bea1090b73ba1fcc63888505b4
developer <maslovaoksanaua@gmail.com>
2017-12-20 16:33:44 +0200

    1) Create adding card page 2) Update coordinates in data

--- src/data/cars.js
+++ src/data/cars.js
@@ -7,24 +7,24 @@ const api = [
                 model: 'Crossover',
                 color: 'red',
                 image: 'http://www.bmw-avangard.ru/images/gallery/gallery_model_cars/cq5damresizedimg1185largetime1455703526909.jpg',
-                latitude: 40.7127753,
-                longitude: -74.0059728
+                latitude: 50.46,
+                longitude: 30.48
             },
             {
                 type: 'X6',
                 model: 'Crossover',
                 color: 'green',
                 image: 'https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/At%20a%20glance/m-sportpackage-slide-01.jpg/_jcr_content/renditions/cq5dam.resized.img.485.low.time1510609710464.jpg',
-                latitude: 40.7127753,
-                longitude: -74.0059728
+                latitude: 50.39,
+                longitude: 30.63
             },
             {
                 type: 'X3',
                 model: 'Crossover',
                 color: 'blue',
                 image: 'https://photos-3.carwow.co.uk/blog/1600/Deep-sea-blue.jpg',
-                latitude: 40.7127753,
-                longitude: -74.0059728
+                latitude: 50.35,
+                longitude: 30.95
             }
         ],
     },
@@ -36,24 +36,24 @@ const api = [
                 model: 'Sedan',
                 color: 'red',
                 image: 'http://carrrsmag.com/data_images/models/mazda-rx-7/mazda-rx-7-02.jpg',
-                latitude: 35.6894875,
-                longitude: 139.69170639999993
+                latitude: 50.27,
+                longitude: 28.67
             },
             {
                 type: 'RX8',
                 model: 'Sedan',
                 color: 'green',
                 image: 'http://www.modelly.com/images/11880/mazda-rx-8-autoart.jpg',
-                latitude: 35.6894875,
-                longitude: 139.69170639999993
+                latitude: 50.30,
+                longitude: 28.01
             },
             {
                 type: 'RX9',
                 model: 'Sedan',
                 color: 'blue',
                 image: 'http://2.bp.blogspot.com/-CtFNBR3r0tU/VGsh3UErC1I/AAAAAAAACIo/MerUTC5Q5kU/s1600/2015%2BMazda%2BRX-9%2BDesign%2BReview.jpg',
-                latitude: 35.6894875,
-                longitude: 139.69170639999993
+                latitude: 50.26,
+                longitude: 30
             }
         ]
     },
@@ -65,24 +65,24 @@ const api = [
                 model: 'Sedan',
                 color: 'red',
                 image: 'http://blog.vehiclejar.com/wp-content/uploads/2014/12/Red-Mercedes-S-Class-by-Brabus-for-Santa-1.jpg',
-                latitude: 52.52000659999999,
-                longitude: 13.404953999999975
+                latitude: 46.47,
+                longitude: 30.70
             },
             {
                 type: 'E',
                 model: 'Sedan',
                 color: 'green',
                 image: 'https://s1.cdn.autoevolution.com/images/news/blue-green-chameleon-wrap-on-mercedes-e-class-44501-7.jpg',
-                latitude: 52.52000659999999,
-                longitude: 13.404953999999975
+                latitude: 46.50,
+                longitude: 28.70
             },
             {
                 type: 'ML',
                 model: 'Crossover',
                 color: 'blue',
                 image: 'https://top-car.ru/upload/gallery/big/p193h1hmepalc1uoo7quel11plv2.jpg',
-                latitude: 52.52000659999999,
-                longitude: 13.404953999999975
+                latitude: 46.02,
+                longitude: 28.75
             }
         ]
     }
--- src/store/reducers/carReducer.js
+++ src/store/reducers/carReducer.js
@@ -8,12 +8,61 @@ export default (state = defaultState, action) => {
     switch (action.type) {
         case CarActions.ACTION_CAR_GET_LIST:
             return {
+                ...state,
                 ...action.payload
             };
         case CarActions.ACTION_CAR_ADD:
+            const {
+                mark,
+                model,
+                year,
+                image,
+                latitude,
+                longitude
+            } = action.payload.data;
+
+            const car = {
+                model,
+                year,
+                image,
+                latitude,
+                longitude
+            };
+
+            const brand = state.list.find((brand) => brand.name === mark);
+
+            if (brand) {
+                const updatedBrand = {
+                    ...brand,
+                    items: [
+                        ...brand.items,
+                        car
+                    ]
+                };
+
+                return {
+                    ...state,
+                    list: state.list.map((item) => {
+                        if (item === brand) {
+                            return updatedBrand;
+                        }
+
+                        return item;
+                    })
+                };
+            }
+
             return {
-                // ...state,
-                // list: action.payload.coordinates
+                ...state,
+                list: [
+                    ...state.list,
+                    {
+                        name: mark,
+                        items: [
+                            car
+                        ]
+                    }
+                ]
             };
         case CarActions.ACTION_CAR_EDIT:
             return {
