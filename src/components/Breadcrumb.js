99d5960c824d8d75b00837bcd7dda9bf609f9eb1
developer <maslovaoksanaua@gmail.com>
2017-12-25 10:04:04 +0200

    Bread crumbs fix.

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import {Link, Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
-const Breadcrumbs = () => (
+class Breadcrumbs extends Component {
+    crumb = (part, partIndex, parts) => {
+        const path = ["", ...parts.slice(0, partIndex + 1)].join("/");
 
-    <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-            <Route
-                path="*"
-                render={props => {
-                    let parts = props.location.pathname.split("/");
-                    const place = parts[parts.length - 1];
-                    parts = parts.slice(1, parts.length - 1);
-                    return <li>{parts.map(crumb)}/{place}</li>;
-                }}
-            />
-        </ol>
-    </nav>
-);
-const crumb = (part, partIndex, parts) => {
-    const path = ["", ...parts.slice(0, partIndex + 1)].join("/");
-    return <Link key={path} to={path}>{part}</Link>;
-};
+        return (
+            <Link key={path} to={path}>{part}</Link>
+        );
+    };
+
+    render() {
+        return (
+            <nav aria-label="breadcrumb">
+                <ol className="breadcrumb">
+                    <Route
+                        path="*"
+                        render={({location: {pathname}}) => {
+                            let parts = pathname.split("/");
+
+                            const place = parts[parts.length - 1];
+                            parts = parts.slice(1, parts.length - 1);
+
+                            return (
+                                <li>{parts.map(this.crumb)}/{place}</li>
+                            );
+                        }}
+                    />
+                </ol>
+            </nav>
+        );
+    }
+}
+
+Breadcrumbs.propTypes = {};
 
 export default Breadcrumbs;
